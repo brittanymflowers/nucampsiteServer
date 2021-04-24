9332ffddec307ac2ad1c6d4fd7ef2a235446cc1c
Megan Flowers <brittanymflowers@gmail.com>
2021-04-24 11:37:42 -0600

    Workshop Week 3

--- routes/promotionRouter.js
+++ routes/promotionRouter.js
@@ -48,7 +48,7 @@ promotionRouter.route('/:promotionId')
             })
             .catch(err => next(err));
     })
-    .post(authenticate.verifyUser, (req, res) => {
+    .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
         res.statusCode = 403;
         res.end(`POST operation not supported on /promotions/${req.params.promotionId}`);
     })
@@ -63,7 +63,7 @@ promotionRouter.route('/:promotionId')
             })
             .catch(err => next(err));
     })
-    .delete(authenticate.verifyUser, (req, res, next) => {
+    .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
         Promotion.findByIdAndDelete(req.params.promotionId)
             .then(response => {
                 res.statusCode = 200;
